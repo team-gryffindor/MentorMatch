0683f96617459f14e4ac2fce2a4e35ebb04edb80
Arjun Logeswaran <arjun.logeswaran121@gmail.com>
2018-09-27 16:45:24 -0400

    React Router setup

---
+++ react-client/src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'; 
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      service: '',
+      location: '',
+    }
+  }
+  
+
+  render () {
+    return (<div>
+      <h1>Mentor Match</h1>
+      <ol>
+        <button><Link to="/login">Login</Link></button>
+      </ol>
+      <input value={this.state.service} onChange={(e) => this.setState({service: e.target.value})} placeholder="Enter Service"/>
+      <input value={this.state.location} onChange={(e) => this.setState({location: e.target.value})} placeholder="Location"/>
+      <button>Search</button>
+    </div>)
+  }
+}
+
+
+export default Home;
