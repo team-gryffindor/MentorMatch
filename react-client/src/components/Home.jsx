9dda8dba22a1a603589e390fde08e3e331930cd5
Alex Charles <alex@Alexs-MacBook-Pro-2.local>
2018-09-27 17:15:24 -0400

    create account view is created. still needs the functionality. Just have to figure out how create account/login works with fire base.

--- react-client/src/components/Home.jsx
+++ react-client/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'; 
 
@@ -17,9 +17,10 @@ class Home extends React.Component {
       <h1>Mentor Match</h1>
       <ol>
         <button><Link to="/login">Login</Link></button>
+        <button><Link to="/signUp">Sign Up</Link></button>
       </ol>
       <input value={this.state.service} onChange={(e) => this.setState({service: e.target.value})} placeholder="Enter Service"/>
-      <input value={this.state.location} onChange={(e) => this.setState({location: e.target.value})} placeholder="Location"/>
+      <input value={this.state.location} onChange={(e) => this.setState({location: e.target.value})} placeholder="Enter Location"/>
       <button>Search</button>
     </div>)
   }
