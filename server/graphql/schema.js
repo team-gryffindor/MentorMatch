19d616d66be32756171364ff1c5db1c99192c0dd
Alan Yang <yangalan17@gmail.com>
2018-09-28 15:43:10 -0400

    working on initial graphql schema

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -2,30 +2,23 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
-const UserType = new GraphQLObjectType({
-  name: 'User',
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    description: { type: GraphQLString },
-    city: { type: GraphQLString },
-    imgString: { type: GraphQLString }
-  })
-});
-
-const LessonType = new GraphQLObjectType({
-  name: 'Lesson',
-  fields: () => ({
-    id: { type: GraphQLID },
-    userId: { type: GraphQLID },
-    name: { type: GraphQLString },
-    description: { type: GraphQLString },
-    difficulty: { type: GraphQLString },
-    category: { type: GraphQLString },
-    location: { type: GraphQLString },
-    imgString: { type: GraphQLString }
-  })
-});
+// dummy data
+const usersEx = [
+  {
+    id: '1',
+    name: 'Raph Nadal',
+    description: 'tennis champion',
+    location: 'NYC',
+    imgString: 'corgi'
+  },
+  {
+    id: '2',
+    name: 'Emma Powell',
+    description: 'expert violinist',
+    location: 'NYC',
+    imgString: 'corgi'
+  }
+];
 
 const lessonsEx = [
   {
@@ -50,21 +43,57 @@ const lessonsEx = [
   }
 ];
 
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    city: { type: GraphQLString },
+    imgString: { type: GraphQLString }
+  })
+});
+
+const LessonType = new GraphQLObjectType({
+  name: 'Lesson',
+  fields: () => ({
+    id: { type: GraphQLID },
+    userId: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    difficulty: { type: GraphQLString },
+    category: { type: GraphQLString },
+    location: { type: GraphQLString },
+    imgString: { type: GraphQLString },
+    user: {
+      type: UserType,
+      resolve(parent, args) {
+        return usersEx.find((user) => {
+          return user.id === parent.userId;
+        });
+      }
+    }
+  })
+});
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     user: {
       type: UserType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // code to get data from db
+        // queries to db
+        return usersEx.find((user) => {
+          return user.id === args.id;
+        });
       }
     },
     lesson: {
       type: LessonType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // code to get data from db
+        // queries to db
         return lessonsEx.find((lesson) => {
           return lesson.id === args.id;
         });
