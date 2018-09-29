a38c80270ada283e9e99ee736d126dff6f592564
myj899 <myj899@gmail.com>
2018-09-29 16:14:51 -0400

    (fix) User table fixed

--- services/server/db/models/user.js
+++ services/server/db/models/user.js
@@ -4,9 +4,17 @@ const user = (sequelize, DataTypes) => {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV1,
       primaryKey: true
-    }
+    },
+    // firebase unique user id
+    uid: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    cityOfResidence: DataTypes.STRING
   });
-
   return User;
 };
 
