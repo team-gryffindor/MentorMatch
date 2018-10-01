4ae49c7213f7498469deaea5b3fb57987d94ca64
Arjun Logeswaran <arjun.logeswaran121@gmail.com>
2018-10-01 19:16:47 -0400

    Apollo Setup

--- react-client/src/components/Dashboard.jsx
+++ react-client/src/components/Dashboard.jsx
@@ -1,39 +1,62 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
 import Navigation from './NavigationBar.jsx';
 import ServiceDisplay from './ServicesHorizontalDisplay.jsx';
 import Search from './Search.jsx';
 
+const getUserInfoQuery = gql`
+  {
+    users {
+      name
+      id
+    }
+  }
+`;
 class Dashboard extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
   }
 
+  displayUsers() {
+    var data = this.props.data;
+    if (data.loading) {
+      return <div>Loading books...</div>;
+    } else {
+      return data.users.map((user) => {
+        return <li>{user.name}</li>;
+      });
+    }
+  }
   render() {
+    console.log(this.props); //checking for apollo query return
+
     return (
       <div>
         <h1>Mentor Match</h1>
+        <ul id="user-list">{this.displayUsers()}</ul>
         <div>
-          <Navigation/>
-          <Search query={this.props.query}/>
+          <Navigation />
+          <Search query={this.props.query} />
         </div>
-        <ServiceOfTheDay service={this.props.service}/>
+        <ServiceOfTheDay service={this.props.service} />
         <div>
           <h2>Favorites</h2>
-          <ServiceDisplay services={this.props.favorites}/>
+          <ServiceDisplay services={this.props.favorites} />
         </div>
       </div>
-    )
+    );
   }
 }
 
-const ServiceOfTheDay = ({service}) => (
+const ServiceOfTheDay = ({ service }) => (
   <div>
-    <img src={service.profilePicture}/>
+    <img src={service.profilePicture} />
     <h1>{service.title}</h1>
     <p>{service.description}</p>
     <button>Book Now</button>
   </div>
 );
 
-
-export default Dashboard;
+export default graphql(getUserInfoQuery)(Dashboard);
