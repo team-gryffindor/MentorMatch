d6f52a25962f9b0b4102d7b4ee5699a8054a9ddc
Alex Charles <alex@Alexs-MacBook-Pro-2.local>
2018-10-01 14:52:53 -0400

    Created header file and added it to different views

--- react-client/src/components/ServiceMoreInfo.jsx
+++ react-client/src/components/ServiceMoreInfo.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Navigation from './NavigationBar'
+import Header from './Header.jsx';
 
 const SderviceMoreInfo = (props) => (
   <div>
-    <h1>Mentor Match</h1>
-    <Navigation/>
-
+    <Header/>
     <h2>{props.service.title}</h2>
     <h3>{props.service.userName}</h3>
     <p>{props.service.description}</p>
