b603dccee54d4e6c916bedaeaee9880600bc2bfe
Alan Yang <yangalan17@gmail.com>
2018-10-02 12:02:33 -0400

    merge

--- server/index.js
+++ server/index.js
@@ -4,12 +4,8 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./graphql/schema.js');
 const config = require('dotenv').config();
 const models = require('./db/index.js');
-<<<<<<< HEAD
-const port = process.env.PORT || 3000;
-=======
 const path = require('path');
 require('dotenv').config();
->>>>>>> dev
 
 const app = express();
 
@@ -26,11 +22,7 @@ app.use(
 );
 
 app.get('/*', function(req, res) {
-<<<<<<< HEAD
-  res.sendfile(path.join(__dirname + '/../react-client/dist/index.html'));
-=======
   res.sendFile(path.join(__dirname + '/../react-client/dist/index.html'));
->>>>>>> dev
 });
 
 models.db
