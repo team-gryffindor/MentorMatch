c29025dcd32b9e522f08583f4485e859b885e5d8
Alex Charles <alex@Alexs-MacBook-Pro-2.local>
2018-10-05 13:25:10 -0400

    navigation bar rendering only login/logout buttons and user can login and out.

--- react-client/src/components/NavigationBar.jsx
+++ react-client/src/components/NavigationBar.jsx
@@ -1,17 +1,40 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom'; 
+import React from 'react';
+import firebase from 'firebase';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'; 
 
 
-// const Navigation = () => (
-//   <div>
-//     <ul>
-//       <li><a><Link to="/active">My Lessons</Link></a></li>
-//       <li><a><Link to="/past">Past Lessons</Link></a></li>
-//       <li><a><Link to="/offered">My Offered Lessons</Link></a></li>
-//       <li><a><Link to="/userProfile">My Profile</Link></a></li>
-//       <li><a><Link to="/addService">Create a new service</Link></a></li>
-//     </ul>
-//   </div>
-// );
+const Navigation = (props) => {
+  let isLoggedIn = <li><a><button onClick={() => {firebase.auth().signOut().then(() => props.handleUserLoggingIn())}}>
+    <Link to='/'>Logout!</Link></button></a></li>;
+  let isLoggedOut = <li><a><button><Link to="/login">Login</Link></button></a></li>
 
-// export default Navigation;
+  if (props.isLoggedIn) {
+    return ( 
+    <div>
+      <ul>
+        {isLoggedIn}
+      </ul>
+    </div>)
+  } else {
+    return (
+    <div>
+      <ul>
+       {isLoggedOut}
+      </ul>
+    </div>
+    )}
+  // return (
+  // <div>
+  //   <ul>
+  //     <li><a><Link to="/active">My Lessons</Link></a></li>
+  //     <li><a><Link to="/past">Past Lessons</Link></a></li>
+  //     <li><a><Link to="/offered">My Offered Lessons</Link></a></li>
+  //     <li><a><Link to="/userProfile">My Profile</Link></a></li>
+  //     <li><a><Link to="/addService">Create a new service</Link></a></li>
+
+  //   </ul>
+  // </div>)
+};
+
+
+export default Navigation;
