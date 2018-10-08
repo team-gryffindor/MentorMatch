c89aa0ee8e4afbcc2c8c5d049d137dbeedd40723
Alex Charles <alex@Alexs-MacBook-Pro-3.local>
2018-10-08 10:49:00 -0400

    Landing component just needs the search componenet and query componenent that gets Lessons with the highest rating

--- react-client/src/components/Home.jsx
+++ react-client/src/components/Home.jsx
@@ -6,27 +6,49 @@ import Navigation from './NavigationBar.jsx';
 import LessonList from './LessonList.jsx';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+//This componenet could be a functional componenet and not requrie storing state at all
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+    };
   }
 
   render() {
+    // return (
+    //   <div>
+    //     <h1>Mentor Match</h1>
+    //     <Navigation
+    //       isLoggedIn={this.props.isLoggedIn}
+    //       handleUserLoggingIn={this.handleUserLoggingIn}
+    //     />
+    //     {/* <Search query={props.query} /> */}
+    //     <h1>Today's Top Services</h1>
+    //     {/* currently grabbing all lessons */}
+    //     <LessonList style="horizontal" />
+    //   </div>
+    // );
+
     return (
       <div>
-        <h1>Mentor Match</h1>
-        <Navigation
-          isLoggedIn={this.props.isLoggedIn}
-          handleUserLoggingIn={this.handleUserLoggingIn}
-        />
-        {/* <Search query={props.query} /> */}
-        <h1>Today's Top Services</h1>
-        {/* currently grabbing all lessons */}
-        <LessonList style="horizontal" />
+        <NavLand />
+        {/* Searchbar component goes here */}
+        <h1>Top Services</h1>
+        {/* Need to change LessonList to display top services not just all lessons in our DB */}
+        <LessonList />
       </div>
-    );
+      
+    )
   }
 }
 
+const NavLand = () => (
+  <div>
+    <h1>Mentor Match</h1>
+    <ul>
+      <li><Link to='/login'>Login</Link></li>
+      <li><Link to='/signup'>Sign Up</Link></li>
+    </ul>
+  </div>
+)
 export default Home;
