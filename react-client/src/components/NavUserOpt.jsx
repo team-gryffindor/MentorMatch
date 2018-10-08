746a1f9d63028afa61e365a25c95a3a6753986d5
Alex Charles <alex@Alexs-MacBook-Pro-3.local>
2018-10-08 17:03:48 -0400

    Added dashbaord link to dropdown menu

--- react-client/src/components/NavUserOpt.jsx
+++ react-client/src/components/NavUserOpt.jsx
@@ -17,7 +17,11 @@ const NavUserOpt = (prop) => (
         >
           <i className="fas fa-user" />
         </a>
+        
         <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a className="dropdown-item" href="#"><Link to='/dashboard'>
+            Dashboard
+            </Link></a>
           <a className="dropdown-item" href="#"><Link to='/userProfile'>
             Profile
             </Link></a>
