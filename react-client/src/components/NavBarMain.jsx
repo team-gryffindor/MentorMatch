6debbb208e42302031c6d206762e008e1e44d674
myj899 <myj899@gmail.com>
2018-10-08 13:26:09 -0400

    (feat) styling for navbar done

--- react-client/src/components/NavBarMain.jsx
+++ react-client/src/components/NavBarMain.jsx
@@ -3,63 +3,28 @@ import firebase from 'firebase';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const NavBarMain = (props) => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">
-      MentorMatch
-    </a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button>
-
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        {/* <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <i className="fas fa-user" />
-          </a>
-          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item" href="#">
-              Action
-            </a>
-            <a className="dropdown-item" href="#">
-              Another action
-            </a>
-            <div className="dropdown-divider" />
-            <a className="dropdown-item" href="#">
-              Something else here
-            </a>
-          </div>
-        </li> */}
-        <li className="navbar-item">
-          <a class="nav-link" href="#">
-            Profile
-          </a>
-        </li>
-        <li className="navbar-item">
-          <a className="nav-link" href="#">
-            <button className="btn btn-outline-secondary my-2 my-sm-0" type="submit">
-              Logout
-            </button>
-            {/* <i className="fas fa-sign-out-alt" /> */}
-          </a>
-        </li>
-      </ul>
+  <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+    <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+      {/* <ul className="navbar-nav mr-auto"> */}
+      {/* <li className="nav-item active"> */}
+      <a className="navbar-brand" href="#">
+        MentorMatch
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      {/* </li> */}
+      {/* </ul> */}
+    </div>
+    <div className="navbar-collapse collapst w-100 mx-auto order-0">
       <form className="form-inline my-2 my-lg-0">
         <input
           className="form-control mr-sm-2"
@@ -77,6 +42,44 @@ const NavBarMain = (props) => (
           <i className="fas fa-search" />
         </button>
       </form>
+    </div>
+    <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+      <ul className="navbar-nav ml-auto">
+        {/* <li className="nav-item">
+          <a className="nav-link" href="#">
+            <i className="fas fa-user" /> Profile
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <button className="btn btn-outline-secondary my-2 my-sm-0" type="submit">
+              Logout
+            </button>
+          </a>
+        </li> */}
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <i className="fas fa-user" />
+          </a>
+          <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a className="dropdown-item" href="#">
+              Profile
+            </a>
+            <div className="dropdown-divider" />
+            <a className="dropdown-item" href="#">
+              Logout
+            </a>
+          </div>
+        </li>
+      </ul>
     </div>
   </nav>
 );
