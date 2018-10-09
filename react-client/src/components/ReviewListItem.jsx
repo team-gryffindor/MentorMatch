dc95fb1eb0cc7b67d747a909bfcbd6edcc50f4b9
myj899 <myj899@gmail.com>
2018-10-08 20:15:59 -0400

    (feat) review list fetches reviewer info and renders image

--- react-client/src/components/ReviewListItem.jsx
+++ react-client/src/components/ReviewListItem.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import StarRatings from 'react-star-ratings';
+// import userImage from '../Default.png';
 
-const ReviewListItem = ({ review }) => (
-  <div>
-    {console.log(review)}
-    <h2>{review.title}</h2>
-    <h4>{review.rating}</h4>
-    <p>{review.comment}</p>
-  </div>
-);
+// TODO: include user info for each review
+// name, cityOfResidence
+const ReviewListItem = ({ review }) => {
+  console.log('REVIEW', review.user);
+  return (
+    <div className="list-group-item flex-column align-items-start">
+      <img
+        src={review.user.image}
+        onError={(e) => {
+          e.target.onerror = null;
+          e.target.src = '../Default.png';
+        }}
+        alt={'reviewer image'}
+        width={100}
+        height={100}
+      />
+      <div>{review.user.name}</div>
+      <div className="d-flex w-100 justify-content-between">
+        <h5 className="mb-1">{review.title}</h5>
+        <small className="text-muted review-margin-left">
+          <StarRatings
+            rating={Number(review.rating.toFixed(2))}
+            starRatedColor="blue"
+            numberOfStars={5}
+            starDimension="15px"
+            starSpacing="1px"
+            name="rating"
+          />
+        </small>
+      </div>
+      <p className="mb-1">{review.comment}</p>
+    </div>
+  );
+};
 
 export default ReviewListItem;
