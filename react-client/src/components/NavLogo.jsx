daa5c9a0c2fb9d63b7d8be0f76435fc60705aa7a
myj899 <myj899@gmail.com>
2018-10-13 15:01:07 -0400

    (feat) Logo no longer logs you out of the page

--- react-client/src/components/NavLogo.jsx
+++ react-client/src/components/NavLogo.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavLogo = (props) => (
   <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-    <a className="navbar-brand" href="/">
-      <img className="logo-img" src={'../MM-Logo-Depth.png'} width={30} />
+    <a className="navbar-brand">
+      <Link to="/">
+        <img className="logo-img" src={'../MM-Logo-Depth.png'} width={30} />
+      </Link>
     </a>
     <button
       className="navbar-toggler"
