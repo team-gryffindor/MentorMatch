3cdaf87bc8f59a2c2397997123ee2a91d2e81954
myj899 <myj899@gmail.com>
2018-10-15 10:07:22 -0400

    temp commit

---
+++ react-client/src/components/authentication/LoginModal.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+class LoginModal extends React.Component {
+  state = {
+    email: '',
+    password: ''
+  };
+
+  render() {
+    let { firebaseApp, uiConfig } = this.props;
+    return (
+      <div
+        className="modal fade"
+        id="loginModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="Login"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebaseApp.auth()} />
+              <div className="strike">
+                <span>or</span>
+              </div>
+              <div className="container">
+                <form>
+                  <div className="form-group">
+                    <label htmlFor="usrname">
+                      <span className="glyphicon glyphicon-user" /> Username
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="usrname"
+                      placeholder="Enter email"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="psw">
+                      <span className="glyphicon glyphicon-eye-open" /> Password
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="psw"
+                      placeholder="Enter password"
+                    />
+                  </div>
+                  <div className="form-group row">
+                    <label className="col-sm-2 col-form-label">Email</label>
+                    <div className="col-sm-10">
+                      <input
+                        type="text"
+                        className="form-control-plaintext"
+                        id="loginEmail"
+                        onChange={(e) => {
+                          this.setState({ email: e.target.value });
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group row">
+                    <label className="col-sm-2 col-form-label">Password</label>
+                    <div className="col-sm-10">
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="loginPassword"
+                        placeholder="Password"
+                        onChange={(e) => {
+                          this.setState({ password: e.target.value });
+                        }}
+                      />
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginModal;
--- react-client/src/components/navbarmain/NavGuestOpt.jsx
+++ react-client/src/components/navbarmain/NavGuestOpt.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import LoginModal from '../authentication/LoginModal.jsx';
+import SignupModal from '../authentication/SignupModal.jsx';
+
 const NavGuestOpt = (props) => (
   <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
     <ul className="navbar-nav ml-auto">
-      <Link className="nav-item" to="/login">
-        <button className="btn btn-secondary my-2 my-sm-0 btn-margin-right" type="submit">
-          Log in
-        </button>
-      </Link>
-      <Link className="nav-item" to="/signup">
-        <button className="btn btn-highlight my-2 my-sm-0 btn-margin-left" type="submit">
-          Sign up
-        </button>
-      </Link>
+      {/* <Link className="nav-item" to="/login"> */}
+      <button
+        className="btn btn-secondary my-2 my-sm-0 btn-margin-right"
+        type="button"
+        data-toggle="modal"
+        data-target="#loginModal"
+      >
+        Log in
+      </button>
+      {/* </Link>
+      <Link className="nav-item" to="/signup"> */}
+      <button
+        className="btn btn-highlight my-2 my-sm-0 btn-margin-left"
+        type="button"
+        data-toggle="modal"
+        data-target="#signupModal"
+      >
+        Sign up
+      </button>
+      {/* </Link> */}
     </ul>
   </div>
 );
