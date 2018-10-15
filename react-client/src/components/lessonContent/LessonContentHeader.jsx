571c6436c4dec5f977c9ce8c8698156291f90e29
Alex Charles <alex@Alexs-MacBook-Pro-3.local>
2018-10-15 12:06:07 -0400

    Book now buttons working the way they are designed to

--- react-client/src/components/lessonContent/LessonContentHeader.jsx
+++ react-client/src/components/lessonContent/LessonContentHeader.jsx
@@ -81,42 +81,12 @@ const LessonContentHeader = ({
           <p>{lesson.provider.description}</p>
         </div>
         <p className="lead text-right">
-          {isBooked ? (
-            <button onClick={() => toggleBooking(false)}>Cancel Booking</button>
-          ) : (
-            <button onClick={() => renderPayment(true)}>Pay Now</button>
-          )}
-          {payNow ? <Checkout userCompletedPayment={userCompletedPayment} lesson={lesson} /> : null}
-
-          {paid ? (
-            <Mutation mutation={mutateBooking}>
-              {(changeBooking) => (
-                <button
-                  className="btn btn-highlight btn-lg"
-                  href="#"
-                  role="button"
-                  onClick={() => {
-                    changeBooking({
-                      variables: {
-                        userId: userId,
-                        lessonId: lesson.id,
-                        date: '1'
-                      }
-                    }).then((data) => {
-                      toggleBooking(!isBooked);
-                    });
-                  }}
-                >
-                  Confirm Booking!
-                </button>
-              )}
-            </Mutation>
-          ) : null}
-          {isBooked ? (
+        {isBooked ? (
             <button onClick={() => toggleBooking(false)}>Cancel Booking</button>
           ) : (
             <BookNow event={lesson} userId={userId} renderPayment={renderPayment} />
           )}
+          {payNow ? <Checkout userCompletedPayment={userCompletedPayment} lesson={lesson} /> : null}
         </p>
       </div>
     </div>
