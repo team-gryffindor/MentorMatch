54f51649143670776735b6738ac31c5cd9fd753d
Alex Charles <alex@Alexs-MacBook-Pro-3.local>
2018-10-16 18:21:11 -0400

    reviews working

--- react-client/src/components/WriteReview.jsx
+++ react-client/src/components/WriteReview.jsx
@@ -1,75 +1,69 @@
 import React from 'react';
 import { ADD_REVIEW } from '../apollo/resolvers/backendQueries';
-import { GET_USER_INFO } from '../apollo/resolvers/clientSideQueries';
-import { Query, Mutation } from 'react-apollo';
+import { Mutation } from 'react-apollo';
+import { Link } from 'react-router-dom';
 
 class WriteReview extends React.Component {
-  state = {
-    title: '',
-    comment: '',
-    rating: 0
-  }
- 
+    state = {
+      title: '',
+      comment: '',
+      rating: 0
+    }
+
   render() {
-    let { lessonId } = this.props;
-    let { title, comment, rating } = this.state;
-    // TODO: need to refetch??
+    let { title, comment, rating} = this.state;
+    let { location } = this.props;
+    let { lesson, userId } = location;
+   
     return (
-      <Query query={GET_USER_INFO}>
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :(</p>;
-          let user = data.userInfo;
-          console.log(lessonId);
-          return (
-            <Mutation mutation={ADD_REVIEW}>
-              {(addReview) => (
-                <div>
-                  <h3>Write a Review!</h3>
-                  <form
-                    onSubmit={(e) => {
-                      e.preventDefault();
-                      addReview({
-                        variables: {
-                          title: title,
-                          comment: comment,
-                          // TODO: restrict user using stars or something
-                          rating: Number(rating),
-                          lessonId: lessonId,
-                          userId: user.userId
-                        }
-                      });
-                    }}
-                  >
-                    Title:
-                    <input
-                      value={title}
-                      onChange={(e) => {
-                        this.setState({ title: e.target.value });
-                      }}
-                    />
-                    Comment:
-                    <input
-                      value={comment}
-                      onChange={(e) => {
-                        this.setState({ comment: e.target.value });
-                      }}
-                    />
-                    Rating:
-                    <input
-                      value={rating}
-                      onChange={(e) => {
-                        this.setState({ rating: e.target.value });
-                      }}
-                    />
-                    <button type="submit">Write Review!</button>
-                  </form>
-                </div>
-              )}
-            </Mutation>
-          );
-        }}
-      </Query>
+        <form>
+          Title:
+          <input
+            placeholder='Title'
+            value={title}
+            onChange={(e) => {
+              this.setState({ title: e.target.value });
+            }}
+          />
+          Comment:
+          <input
+            placeholder='Comment'
+            value={comment}
+            onChange={(e) => {
+              this.setState({ comment: e.target.value });
+            }}
+          />
+          Rating:
+          <input
+            value={rating}
+            onChange={(e) => {
+              this.setState({ rating: e.target.value });
+            }}
+          />
+          <Mutation mutation={ADD_REVIEW}>
+            {(addReview) => (
+              <button onClick={(e) => {
+                e.preventDefault()
+                addReview({
+                  variables: {
+                    title,
+                    comment,
+                    rating: Number(rating),
+                    lessonId: lesson.lesson.id,
+                    userId: userId.userId
+                  }
+                })
+                this.setState({
+                  title: '',
+                  comment: '',
+                  rating: 0
+                })
+                alert('Thank you for sending in your review!');
+              }}>Submit Review</button>
+            )}
+          
+          </Mutation>
+        </form>
     );
   }
 }
