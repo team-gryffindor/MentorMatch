755c04e2bf5b52af9411724033c46054ddeeb320
myj899 <myj899@gmail.com>
2018-10-17 09:44:40 -0400

    write review redirects

--- react-client/src/components/WriteReview.jsx
+++ react-client/src/components/WriteReview.jsx
@@ -4,66 +4,74 @@ import { Mutation } from 'react-apollo';
 import { Link } from 'react-router-dom';
 
 class WriteReview extends React.Component {
-    state = {
-      title: '',
-      comment: '',
-      rating: 0
-    }
+  state = {
+    title: '',
+    comment: '',
+    rating: 0
+  };
 
   render() {
-    let { title, comment, rating} = this.state;
+    let { title, comment, rating } = this.state;
     let { location } = this.props;
     let { lesson, userId } = location;
-   
+
     return (
-        <form>
-          Title:
-          <input
-            placeholder='Title'
-            value={title}
-            onChange={(e) => {
-              this.setState({ title: e.target.value });
-            }}
-          />
-          Comment:
-          <input
-            placeholder='Comment'
-            value={comment}
-            onChange={(e) => {
-              this.setState({ comment: e.target.value });
-            }}
-          />
-          Rating:
-          <input
-            value={rating}
-            onChange={(e) => {
-              this.setState({ rating: e.target.value });
-            }}
-          />
-          <Mutation mutation={ADD_REVIEW}>
-            {(addReview) => (
-              <button onClick={(e) => {
-                e.preventDefault()
-                addReview({
-                  variables: {
-                    title,
-                    comment,
-                    rating: Number(rating),
-                    lessonId: lesson.lesson.id,
-                    userId: userId.userId
-                  }
-                })
-                this.setState({
-                  title: '',
-                  comment: '',
-                  rating: 0
-                })
-                alert('Thank you for sending in your review!');
-              }}>Submit Review</button>
-            )}
-          
-          </Mutation>
-        </form>
+      <form>
+        Title:
+        <input
+          placeholder="Title"
+          value={title}
+          onChange={(e) => {
+            this.setState({ title: e.target.value });
+          }}
+        />
+        Comment:
+        <input
+          placeholder="Comment"
+          value={comment}
+          onChange={(e) => {
+            this.setState({ comment: e.target.value });
+          }}
+        />
+        Rating:
+        <input
+          value={rating}
+          onChange={(e) => {
+            this.setState({ rating: e.target.value });
+          }}
+        />
+        <Mutation
+          mutation={ADD_REVIEW}
+          refetchQueries={[{ query: GET_USER, variables: { id: userId } }]}
+        >
+          {(addReview) => (
+            <Link to={{ pathname: '/lessonContent/:lessonId', state: { lesson: lesson } }}>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  addReview({
+                    variables: {
+                      title,
+                      comment,
+                      rating: Number(rating),
+                      lessonId: lesson.lesson.id,
+                      userId: userId.userId
+                    }
+                  });
+                  this.setState({
+                    title: '',
+                    comment: '',
+                    rating: 0
+                  });
+                  alert('Thank you for sending in your review!');
+                }}
+              >
+                Submit Review
+              </button>
+            </Link>
+          )}
+        </Mutation>
+      </form>
     );
   }
 }
