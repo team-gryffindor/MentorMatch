01419f085b886b1b357d8a276be938acc960d918
myj899 <myj899@gmail.com>
2018-10-17 09:30:57 -0400

    refetch after canceling booking done

--- react-client/src/components/lessonContent/CancelNow.jsx
+++ react-client/src/components/lessonContent/CancelNow.jsx
@@ -3,33 +3,33 @@ import { Mutation } from 'react-apollo';
 import { DELETE_SIGNUP_LESSON } from '../../apollo/resolvers/backendQueries.js';
 
 const CancelNow = ({ userId, toggleBooking, lesson }) => (
-
-    
   <Mutation
     mutation={DELETE_SIGNUP_LESSON}
-   >
-      {(deleteSignupLesson) => (
-        <button
-          type="button"
-          className="btn btn-default"
-          onClick={() => {
-            deleteSignupLesson({
-              variables: {
-                userId: userId,
-                lessonId: lesson.id
-              }
-            }).then((data) => {
+    refetchQueries={[{ query: GET_USER, variables: { id: userId } }]}
+  >
+    {(deleteSignupLesson) => (
+      <button
+        type="button"
+        className="btn btn-default"
+        onClick={() => {
+          deleteSignupLesson({
+            variables: {
+              userId: userId,
+              lessonId: lesson.id
+            }
+          })
+            .then((data) => {
               toggleBooking(false);
-            }).catch((err) => {
-              console.error('Error inside CancelNow Component', err);
             })
-          }}
-        >
-          Cancel Booking
-        </button>
-      )}
-    </Mutation>
-)
+            .catch((err) => {
+              console.error('Error inside CancelNow Component', err);
+            });
+        }}
+      >
+        Cancel Booking
+      </button>
+    )}
+  </Mutation>
+);
 
 export default CancelNow;
-
