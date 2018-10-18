c6655aeb1e3a2a9ecbcb95c85c40d568d33f0502
Alex Charles <alex@Alexs-MacBook-Pro-3.local>
2018-10-17 22:34:41 -0400

    testing

--- react-client/src/components/navbarmain/NavBarMain.jsx
+++ react-client/src/components/navbarmain/NavBarMain.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 import NavLogo from './NavLogo.jsx';
@@ -8,15 +9,20 @@ import NavUserOpt from './NavUserOpt.jsx';
 const NavBarMain = (props) => (
   <div>
     <div className="navBar-css">
+      {/* <img className="logo-img" src={'../withText.png'} width={80} /> */}
+
       <nav
         className="navbar navbar-expand-md navbar-light"
         style={{ backgroundColor: 'transparent' }}
       >
-        <img className="logo-img" src={'../withText.png'} width={80} />
         {props.currentPath !== '/' && <NavLogo />}
         {props.currentPath !== '/' && <NavSearchBar />}
         {props.isLoggedIn ? (
-          <NavUserOpt handleLogin={props.handleLogin} apolloClient={props.apolloClient} />
+          <NavUserOpt
+            userImg={props.userImg}
+            handleLogin={props.handleLogin}
+            apolloClient={props.apolloClient}
+          />
         ) : (
           <NavGuestOpt handleGuestOpt={props.handleGuestOpt} />
         )}
