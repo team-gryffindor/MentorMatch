93efeb4684b096b2a12d2e7d7a66a493625963ad
Arjun Logeswaran <arjun.logeswaran121@gmail.com>
2018-10-19 11:36:46 -0400

    CSS complete for Stripe

--- react-client/src/components/checkout/CardForm.jsx
+++ react-client/src/components/checkout/CardForm.jsx
@@ -1,165 +1,3 @@
-// import React from 'react';
-// import axios from 'axios';
-// import { Redirect } from 'react-router-dom';
-
-// import { CardNumberElement,
-//         CardExpiryElement,
-//         CardCVCElement,
-//         PostalCodeElement, 
-//         injectStripe 
-//       } from 'react-stripe-elements';
-
-//       const handleBlur = () => {
-//         console.log('[blur]');
-//       };
-//       const handleChange = (change) => {
-//         console.log('[change]', change);
-//       };
-//       const handleClick = () => {
-//         console.log('[click]');
-//       };
-//       const handleFocus = () => {
-//         console.log('[focus]');
-//       };
-//       const handleReady = () => {
-//         console.log('[ready]');
-//       };
-
-//       const createOptions = (fontSize, padding) => {
-//         return {
-          
-//             iconStyle: 'solid',
-//             style: {
-//               base: {
-//                 iconColor: '#8898AA',
-//                 color: 'white',
-//                 lineHeight: '36px',
-//                 color: '#0078e0',
-//                 fontWeight: 300,
-//                 fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-//                 fontSize: '19px',
-          
-//                 '::placeholder': {
-//                   color: '#8898AA',
-//                 },
-//               },
-//               invalid: {
-//                 iconColor: '#e85746',
-//                 color: '#e85746',
-//               }
-//             },
-//             classes: {
-//               focus: 'is-focused',
-//               empty: 'is-empty',
-//             },
-//           }
-//         };
-    
-        
-      
-
-// class _SplitForm extends React.Component {
-//   handleSubmit = (ev) => {
-//     ev.preventDefault();
-//     if (this.props.stripe) {
-//       this.props.stripe
-//         .createToken()
-//         .then(payload => {
-//           console.log(payload.token.id)
-//           console.log('this is lesson', this.props.lesson)
-//           axios.post("/charge", {
-//             // stripeEmail: 'arjun. logeswaran121@gmail.com',
-//             stripePrice: this.props.lesson.price,
-//             stripeToken: payload.token.id
-//           })
-//           .then((response) => {
-//             this.props.redirectToCalendar()
-//           })
-//           .then(() => {
-//             this.props.userCompletedPayment(true);
-//             alert(`You just paid ${this.props.lesson.provider.name} $${this.props.lesson.price}!`);
-//           })
-//           .catch((err) => {
-//             console.error('Error in handlePaymentSubmit/cardForm', err);
-//             alert('Something went wrong with your payment, please try again!')
-//           })
-
-//         })
-//         .catch((err) => {
-//           console.error('Error in submitPayment, final catch', err)
-//         })
-//     } else {
-//       console.log("Stripe.js hasn't loaded yet.");
-//     }
-//   };
-
-  
-  
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit} id="payment-form" className="cardForm">
-//       <div >
-//         <label>
-//           Card number
-//           <div >
-//           <CardNumberElement 
-//             onBlur={handleBlur}
-//             onChange={handleChange}
-//             onFocus={handleFocus}
-//             onReady={handleReady}
-//             {...createOptions(this.props.fontSize)}
-//           />
-//           </div>
-//         </label>
-   
-//         <label>
-//           Expiration date
-//           <div >
-//           <CardExpiryElement
-//             onBlur={handleBlur}
-//             onChange={handleChange}
-//             onFocus={handleFocus}
-//             onReady={handleReady}
-//             {...createOptions(this.props.fontSize)}
-//           />
-//           </div>
-//         </label>
-   
-//         <label>
-//           CVC
-//           <div >
-//           <CardCVCElement
-//             onBlur={handleBlur}
-//             onChange={handleChange}
-//             onFocus={handleFocus}
-//             onReady={handleReady}
-//             {...createOptions(this.props.fontSize)}
-//           />
-//           </div>
-//         </label>
-   
-//         <label>
-//           Postal code
-//           <div >
-//           <PostalCodeElement
-//             onBlur={handleBlur}
-//             onChange={handleChange}
-//             onFocus={handleFocus}
-//             onReady={handleReady}
-//             {...createOptions(this.props.fontSize)}
-//           />
-//           </div>
-//         </label>
-//         <br/>
-//         <button>Pay</button>
-//         </div>
-//       </form>
-//     );
-//   }
-// }
-// const SplitForm = injectStripe(_SplitForm);
-// export default SplitForm
-
 import React from 'react';
 import axios from 'axios';
 import Modal from 'react-awesome-modal';
@@ -194,21 +32,18 @@ import { CardNumberElement,
             iconStyle: 'solid',
             style: {
               base: {
-                iconColor: '#8898AA',
-                color: 'white',
-                lineHeight: '36px',
-                color: '#0078e0',
-                fontWeight: 300,
+                color: '#f3f7fa',
+                lineHeight: '18px',
                 fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-                fontSize: '19px',
-          
+                fontSmoothing: 'antialiased',
+                fontSize: '16px',
                 '::placeholder': {
-                  color: '#8898AA',
-                },
+                  color: '#f3f7fa'
+                }
               },
               invalid: {
-                iconColor: '#e85746',
-                color: '#e85746',
+                color: '#fa755a',
+                iconColor: '#fa755a'
               }
             },
             classes: {
@@ -276,22 +111,20 @@ class _SplitForm extends React.Component {
   
   render() {
   return(
-    
+   
     <section>
-    <input type="button" value="Open" onClick={() => this.openModal()} />
-    <Modal visible={this.state.visible} width="500" height="500" effect="fadeInUp" onClickAway={() => this.closeModal()}>
-        <div className="container" >
-            
-      <h3>Mentor Match Automated Payment...by Stripe</h3>
+    <Modal visible={this.state.visible}  width="500" height="375" effect="fadeInUp" onClickAway={() => this.closeModal()}>
+        
+    <div className="container" className="rcorners2" style={{backgroundColor: '#60aff5'}}>
+      <h4>MentorMatchPayment...by Stripe</h4>
+      {/* 60aff5 */}
       
-  
-      <form onSubmit={this.handleSubmit} id="payment-form" className="cardForm">
-      <div >
-      <div className="form-group">
-        <label>
+    
+      <form onSubmit={this.handleSubmit} id="payment-form" >
+      <div className="form-row" id="card-element">
+        <label for="card-element">
           Card number
-          <CardNumberElement 
-            className="form-control"
+          <CardNumberElement className="stripe-search-btn-width"
             onBlur={handleBlur}
             onChange={handleChange}
             onFocus={handleFocus}
@@ -300,11 +133,10 @@ class _SplitForm extends React.Component {
           />
         </label>
       </div>
-      <div className="form-group">
-        <label>
+      <div className="form-row" id="card-element">
+        <label for="card-element">
           Expiration date
           <CardExpiryElement
-            className="form-control"
             onBlur={handleBlur}
             onChange={handleChange}
             onFocus={handleFocus}
@@ -313,11 +145,10 @@ class _SplitForm extends React.Component {
           />
         </label>
       </div>
-      <div className="form-group">
-        <label>
+      <div className="form-row">
+        <label for="card-element">
           CVC
-          <CardCVCElement
-            className="form-control"
+          <CardCVCElement className="stripe-cvc-btn-width"
             onBlur={handleBlur}
             onChange={handleChange}
             onFocus={handleFocus}
@@ -326,11 +157,10 @@ class _SplitForm extends React.Component {
           />
         </label>
       </div>
-      <div className="form-group">
-        <label>
+      <div className="form-row">
+        <label for="card-element">
           Postal code
           <PostalCodeElement
-            className="form-control"
             onBlur={handleBlur}
             onChange={handleChange}
             onFocus={handleFocus}
@@ -340,12 +170,13 @@ class _SplitForm extends React.Component {
         </label>
         </div>
         <br/>
-        <a href="javascript:void(0);" onClick={() => this.closeModal()}><button>Pay</button></a>
-        </div>
+        <a href="javascript:void(0);" onClick={() => this.closeModal()}><button className="btn-outline-stripe">Submit Payment!</button></a>
       </form>
-        </div>
+      </div>
     </Modal>
   </section>
+
+
     );
   }
 }
